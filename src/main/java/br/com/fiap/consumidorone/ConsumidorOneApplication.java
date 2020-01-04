f00eb91127b02d5fb6b2414bacc787d9987413af
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-01-04 16:39:22 -0300

    Commit inicial

---
+++ src/main/java/br/com/fiap/consumidorone/ConsumidorOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.consumidorone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumidorOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumidorOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/consumidorone/ConsumidorOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.consumidorone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumidorOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
