3d09b8d8467d1331015ed817e865b2e72d4f5331
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-01-15 19:55:46 -0300

    Correções para test

--- src/main/java/br/com/fiap/consumidorone/components/KafkaConsumer.java
+++ src/main/java/br/com/fiap/consumidorone/components/KafkaConsumer.java
@@ -3,10 +3,12 @@ package br.com.fiap.consumidorone.components;
 import br.com.fiap.consumidorone.domain.BolsaFamiliaModel;
 import br.com.fiap.consumidorone.services.ConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component(value = "kafkaConsumer")
+@Profile(value = "default")
 public class KafkaConsumer implements Consumer{
 
     @Autowired
