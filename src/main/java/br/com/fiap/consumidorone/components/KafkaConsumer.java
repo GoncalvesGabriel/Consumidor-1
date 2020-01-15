aaf59869f82d85d1c473ce8a250d9f83a4ee2650
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-01-15 19:44:41 -0300

    Correções para test

---
+++ src/main/java/br/com/fiap/consumidorone/components/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package br.com.fiap.consumidorone.components;
+
+import br.com.fiap.consumidorone.domain.BolsaFamiliaModel;
+import br.com.fiap.consumidorone.services.ConsumerService;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @Autowired
+    private ConsumerService service;
+
+    @KafkaListener(topics = "${consumerone.topic}", groupId = "${consumerone.group-id}")
+    public void consume(final BolsaFamiliaModel consumerRecord) {
+            service.consume(consumerRecord);
+    }
+}
